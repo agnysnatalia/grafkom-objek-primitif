360fc16701b8d1570e2d1e07031914ef49109795
agnysnatalia <56225587+agnysnatalia@users.noreply.github.com>
2021-02-23 20:49:26 +0700

    Add files via upload

---
+++ lingkaran1.cpp
@@ -0,0 +1,54 @@
+#include <GL/glut.h>
+#include<math.h>
+int i=0; int j=0;
+void lingkaran(int xp, int yp, int r, int n){
+    float a,x,y;
+    glColor4ub(53,0,6,32);
+    glBegin(GL_POLYGON);
+        a=6.28/n;
+        for (int i=0; i<n; i++){
+            x = xp+r * cos(i*a);
+            y = yp+r * sin(i*a);
+            glVertex2d(x,y);
+        }
+    glEnd();
+}
+ 
+static void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); //untuk membuat opacity
+    glColor3ub(255,0,0);
+    glBegin(GL_LINES);
+        glVertex2d(-500,0);
+        glVertex2d(500,0);
+        glVertex2d(0,400);
+        glVertex2d(0,-400);
+    glEnd();
+ 
+    lingkaran(0,0,100,25);
+    i+=20;
+    j+=1;
+    glutSwapBuffers();
+}
+ 
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitWindowSize(600,400);
+    glutInitWindowPosition(10,10);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+ 
+    glutCreateWindow("GLUT Shapes");
+ 
+    gluOrtho2D(-500,500,-400,400);
+ 
+ 
+    glutDisplayFunc(display);
+    glutIdleFunc(display);
+ 
+    glClearColor(1,1,1,1);
+    glutMainLoop();
+}
+
+/*Imanuela Gloriagnys Natalia / 19051397032 / D4 MI 19 A */
